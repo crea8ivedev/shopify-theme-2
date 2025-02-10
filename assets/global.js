637b6c09a3309680e8d1c5f2d8616b45773b61cd
JayP <jayp.encircle@gmail.com>
2025-02-10 19:24:10 +0530

    devloping: header

---
+++ assets/global.js
@@ -0,0 +1,69 @@
+// Header account-popup 
+$(".login_popup").click(function () {
+    if ($(this).hasClass("active")) {
+      // If the login popup is already active, we should close it
+      $(this).removeClass("active");
+      $(".customer-account").addClass("hidden");
+      $(".customer-account").removeClass("login-left");
+      $("body").removeClass("overflow-hidden");
+      $("#shopify-section-account-popup").hide();
+      $(".ctm-login-modal-bg").removeClass("active");
+    } else {
+      // If the login popup is not active, we should open it
+      $(this).addClass("active");
+      $(".customer-account").removeClass("hidden");
+      $("#shopify-section-account-popup").show();
+      $(".ctm-login-modal-bg").addClass("active");
+      $(".customer-account").addClass("login-left");
+      $("body").addClass("overflow-hidden");
+    }
+  });
+  
+  $(".ctm-login-modal-bg").click(function () {
+    $(this).removeClass("active");
+    $(".customer-account").addClass("hidden");
+    $(".customer-account").removeClass("login-left");
+    $(".login_popup").removeClass("active");
+    $("body").removeClass("overflow-hidden");
+    $("#shopify-section-account-popup").hide();
+  });
+  
+  $("#login-none").click(function () {
+    $(".login-cta").addClass("d-none");
+    $(".reset-password").removeClass("d-none");
+  });
+  $("#recover-cancel").click(function () {
+    $(".login-cta").removeClass("d-none");
+    $(".reset-password").addClass("d-none");
+  });
+  
+  $("#regi_forgot_pass").click(function () {
+    $("#img-1").addClass("current");
+    $("#tab-1").addClass("current");
+    $("#login").addClass("current");
+    $("#img-2").removeClass("current");
+    $("#tab-2").removeClass("current");
+    $("#signup").removeClass("current");
+  
+  });
+  $("#regi-recover-cancel").click(function () {
+    $(".reset-password").addClass("d-none");
+    $(".forgot-block-area").removeClass("d-none");
+  });
+  
+  // popup tabbing
+  $(".tab-heading ul li a").click(function () {
+    var tab_idd = $(this).attr("data-tab");
+    var tab_img = $(this).attr("data-img");
+  
+    $(".reset-password").addClass("d-none");
+    $(".login-cta").removeClass("d-none");
+    $(".forgot-block-area").removeClass("d-none");
+  
+    $(".tab-heading ul li a").removeClass("current");
+    $(".account-tab.tab-pane").removeClass("current");
+  
+    $(this).addClass("current");
+    $("#" + tab_idd).addClass("current");
+    $("#" + tab_img).addClass("current");
+  });
