2186f215e24ebc69ebe1acac823fa9b7488837ac
DeexaBET <deexab.encircle@gmail.com>
2025-04-16 16:46:39 +0530

    refactor: zigzag section

---
+++ assets/media-with-text.js
@@ -0,0 +1,57 @@
+function handleMediaInteraction(e) {
+    e.preventDefault();
+    const videoContainer = e.target.closest(".video-with-text-container");
+    if (
+      !videoContainer ||
+      !(e.type === "click" || e.key === "Enter" || e.key === " ")
+    )
+      {return;}
+  
+    const videoWithTextCover = videoContainer.querySelector(
+      ".video-with-text-cover",
+    );
+    const videoIframe = videoContainer.querySelector("iframe#video-player");
+    const video = videoContainer.querySelector("video");
+    const videoPlayIcon = videoContainer.querySelector(".video-play-button");
+  
+    if (videoWithTextCover) {
+        videoWithTextCover.classList.add("hidden");}
+        
+        if (videoIframe) {
+        console.log("videoIframe",videoIframe)
+      videoIframe.classList.remove("hidden");
+      videoIframe.classList.add("visible");
+  
+      if (!videoIframe.src.includes("&autoplay=1")) {
+        videoIframe.src += "&autoplay=1";
+        videoIframe.setAttribute("tabindex", "0");
+        videoIframe.focus();
+      }
+    }
+  
+    if (video) {
+          video.play();
+          if (videoPlayIcon) {
+            videoPlayIcon.classList.add("hidden");
+            videoPlayIcon.classList.remove("visible");
+          }
+        } else {
+          video.pause();
+          if (videoPlayIcon) {
+            videoPlayIcon.classList.remove("hidden");
+            videoPlayIcon.classList.add("visible");
+          }
+        }
+      
+        video.setAttribute("tabindex", "0");
+        video.focus();
+      }
+    
+  
+  document.addEventListener("click", handleMediaInteraction);
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "Enter" || e.key === " ") {
+      handleMediaInteraction(e);
+    }
+  });
+  
