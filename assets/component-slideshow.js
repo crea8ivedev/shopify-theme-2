423d9dd7ea50fc26a61e2b27938cf71854a99d28
AbhiDET <Abhishekd.encircle@gmail.com>
2025-05-12 13:31:22 +0530

    fix: marquee json schema

--- assets/component-slideshow.js
+++ assets/component-slideshow.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  new Swiper(".slidshow-swiper", {});
+  const sliders = document.querySelectorAll(".slideshow-swiper");
+
+  sliders.forEach((sliderEl) => {
+    const autoplay = sliderEl.dataset.autoplay === "true";
+
+    const options = {
+      loop: true,
+    };
+
+    if (autoplay) {
+      const speed = (parseFloat(sliderEl.dataset.speed) || 3) * 1000;
+      options.autoplay = { delay: speed, disableOnInteraction: false };
+      options.speed = 1500;
+    }
+
+    new Swiper(sliderEl, options);
+  });
 });
